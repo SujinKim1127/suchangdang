506955b112e8f0e18365975135cbd3ce344dd1e4
SuJin <kimsujin001127@gmail.com>
2024-08-08 20:31:59 +0900

    feat: 이미지 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import first from "./images/001.png";
+import second from "./images/002.png";
+import third from "./images/003.png";
+import fourth from "./images/004.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Img src={first} />
+      <Img src={second} />
+      <Img src={third} />
+      <Img src={fourth} />
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #b8e9ff;
+  max-width: 100vw;
+`;
+
+const Img = styled.img`
+  width: 700px;
+`;
 
 export default App;
