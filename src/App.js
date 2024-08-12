fd597a5c56673958c8c4d1ede68527a36946a9c3
SuJin <kimsujin001127@gmail.com>
2024-08-12 14:15:08 +0900

    feat: 위치 문구 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import first from "./images/001.png";
 import second from "./images/002.png";
 import third from "./images/003.png";
 import fourth from "./images/004.png";
+import six from "./images/006.png";
 import Map from "./Map";
 import { Container as MapDiv, NavermapsProvider } from "react-naver-maps";
 
@@ -26,7 +27,9 @@ function App() {
               <Map />
             </MapDiv>
           </MapWrapper>
+          <PosText>서울 강남구 학동로82길 17 금은빌딩 1층</PosText>
         </WhiteBox>
+        <Img src={six} />
       </Wrapper>
     </NavermapsProvider>
   );
@@ -54,7 +57,7 @@ const WhiteBox = styled.div`
   @media screen and (min-width: 780px) {
     width: 700px;
   }
-
+  padding-bottom: 30px;
   width: 90%;
 `;
 
@@ -70,6 +73,14 @@ const MapWrapper = styled.div`
 
   width: 60%;
   height: 150px;
+`;
+
+const PosText = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  font-weight: 600;
 `;
 
 export default App;
